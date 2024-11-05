8d5c8eaf27a1929275dd42375b59634386b5b03f
Hendrik Ebbers <hendrik.ebbers@web.de>
2024-11-05 13:10:58 +0100

    python results

--- src/main/java/com/openelements/oss/license/scanner/MainCommand.java
+++ src/main/java/com/openelements/oss/license/scanner/MainCommand.java
@@ -3,7 +3,7 @@ package com.openelements.oss.license.scanner;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-@Command(description = "A tool to scan a library for all dependencies", subcommands = {JavaCommand.class, JavaScriptCommand.class, RustCommand.class, SwiftCommand.class, GoCommand.class})
+@Command(description = "A tool to scan a library for all dependencies", subcommands = {JavaCommand.class, JavaScriptCommand.class, RustCommand.class, SwiftCommand.class, GoCommand.class, PythonCommand.class})
 public class MainCommand {
 
     public static void main(String[] args) {
